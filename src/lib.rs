8e1734175f6fa281f8600803de45f2870ae828ca
Lothar <ldroppelmann@uc.cl>
2023-08-06 14:28:23 -0400

    feat: random english words in test

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
+use serde_json;
+use serde_json::Value;
+use std::fs::File;
+use std::io::BufReader;
 use std::io::Write;
 use std::time::Duration;
+use rand::seq::IteratorRandom;
 
 mod test_state;
 use test_state::{Key, TestState};
@@ -52,7 +57,7 @@ impl ChimpType {
     fn handle_key(&mut self, key: Key) {
         match self.state {
             State::Welcome if key == Key::Enter => {
-                let word_list = self.generate_word_list();
+                let word_list = self.generate_word_list(25);
                 self.state = State::RunningTest(TestState::new(&word_list));
             }
             State::Welcome if key == Key::Esc => {
@@ -107,12 +112,22 @@ impl ChimpType {
         self.io_controller.flush();
     }
 
-    fn generate_word_list(&self) -> Vec<String> {
-        let words: Vec<&str> = vec![
-            "dog", "cat", "sun", "book", "tree", "ball", "happy", "red", "car", "run", "bird",
-            "door", "baby", "hat", "song", "fish", "pen", "bed", "star", "milk",
-        ];
-        words.iter().map(|s| s.to_string()).collect()
+    fn generate_word_list(&self, n: usize) -> Vec<String> {
+        let buf = BufReader::new(File::open("./languages/english.json").unwrap());
+        let json: Value = serde_json::from_reader(buf).unwrap();
+        if let Value::Array(words) = &json["words"] {
+            let words = words.iter().choose_multiple(&mut rand::thread_rng(), n);
+            return words
+                .iter()
+                .map(|s| {
+                    match s {
+                        Value::String(s) => s.to_owned(),
+                        _ => panic!("Non string in words array"),
+                    }
+                })
+                .collect();
+        }
+        panic!("Invalid json");
     }
 
     fn draw_test_complete(&mut self) {
